909bfe7150f1df6c52efa44c8de9401eaba2e6e0
MaybeMaru <97055307+MaybeMaru@users.noreply.github.com>
2025-03-28 23:06:44 +0100

    reduce copy pasted stuff

---
+++ js/common.js
@@ -0,0 +1,68 @@
+function initCommon()
+{
+    // shader bg
+    document.body.insertAdjacentHTML('afterbegin', `
+        <canvas id="shaderCanvas"></canvas>
+        <script src="js/snowShader.js"><\/script>
+    `);
+
+    // header
+    document.body.insertAdjacentHTML('afterbegin', `
+    <header>
+        <div class="title">MaybeMaru</div>
+        <nav class="header-buttons">
+            <a href="index.html">Home</a>
+            <a href="art.html">Art</a>
+            <a href="code.html">Code</a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+    </header>
+    `);
+    
+    // TODO: remove this when shit is done
+    document.body.insertAdjacentHTML('beforeend', `
+    <img src="https://www.brendoncolby.com/images/under_construction.gif"
+    class="construction-gif">
+    `);
+    
+    highlightCurrentPage();
+}
+
+// highlight current page in header
+function highlightCurrentPage() {
+    const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+    const links = document.querySelectorAll('.header-buttons a');
+    
+    links.forEach(link => {
+        const linkPage = link.getAttribute('href');
+        if (currentPage === linkPage) {
+            link.classList.add('current-page');
+        }
+    });
+}
+
+// init home slideshow
+function initSlideshow() {
+    const slideshow = document.querySelector('.slideshow-container');
+    if (!slideshow) return;
+    
+    let index = 0;
+    const images = document.querySelectorAll(".slideshow-container img");
+    
+    function fadeImages() {
+        images[index].classList.remove("active");
+        index = (index + 1) % images.length;
+        images[index].classList.add("active");
+    }
+    
+    if (images.length > 0) {
+        setInterval(fadeImages, 3000);
+    }
+}
+
+// call when site is opened
+document.addEventListener('DOMContentLoaded', () => {
+    initCommon();
+    initSlideshow();
+});
